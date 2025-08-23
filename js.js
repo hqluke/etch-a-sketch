78dd83d036d551e0c1485ae17866e7f475544e40
Your Name <212039062+hqluke@users.noreply.github.com>
2025-08-22 18:26:59 -0600

    Elements completed. Next up is da css

--- js.js
+++ js.js
@@ -1,14 +1,20 @@
 const body = document.body
 const containerDiv = document.querySelector(".container");
 let currentColor = document.querySelector(".palette") //acess it with  currentColor.value
-let gridSize = "16x16";
+let gridSize = "16";
+let newGridSize = "";
+
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log("hi")
 
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("titleDiv");
+    body.insertBefore(titleDiv, containerDiv);
+
     const title = document.createElement("h1");
-    title.textContent = `Etch-a-Sketch ${gridSize}`;
-    body.appendChild(title);
+    title.textContent = `Etch-a-Sketch ${gridSize}x${gridSize}`;
+    titleDiv.appendChild(title);
 
     const elementsDiv = document.createElement("div");
     elementsDiv.classList.add("elements");
@@ -38,7 +44,8 @@ document.addEventListener('DOMContentLoaded', function() {
     rangeSlider.classList.add("rangeSlider");
     rangeSlider.min = 4;
     rangeSlider.max = 100;
-    rangeSlider.step = 4
+    rangeSlider.step = 4;
+    rangeSlider.value = 16;
     sizeDiv.appendChild(rangeSlider);
 
     const rangeValue = document.createElement("p");
@@ -50,8 +57,25 @@ document.addEventListener('DOMContentLoaded', function() {
     rangeButton.textContent = "Create New Grid";
     sizeDiv.appendChild(rangeButton);
 
-    
+    const resetDiv = document.createElement("div");
+    resetDiv.classList.add("resetDiv");
+    elementsDiv.appendChild(resetDiv);
 
+    const resetButton = document.createElement("button");
+    resetButton.classList.add("resetButton");
+    resetButton.textContent = "Reset Current Grid";
+    resetDiv.appendChild(resetButton);
+
+    const rulesDiv = document.createElement("div");
+    rulesDiv.classList.add("rulesDiv");
+    elementsDiv.appendChild(rulesDiv);
+
+    const daRulez = document.createElement("p");
+    daRulez.classList.add("daRulez");
+    daRulez.textContent = "When you move the mouse over the squares, they get colored in. You can reset the current grid or create a new grid with your desired size; max size is 100x100. You can turn a square back to white by holding alt and hovering over the square with your mouse. Holding shift will prevent you from coloring so you can get to specific squares."
+    rulesDiv.appendChild(daRulez);
+    
+    
 
 
     const gridDiv = document.createElement("div");
@@ -70,17 +94,33 @@ document.addEventListener('DOMContentLoaded', function() {
 function slider(){
 const rSlider = document.querySelector(".rangeSlider");
 const rValue = document.querySelector(".rangeValue");
-const rButton = document.querySelector(".rangeButton");
-let rangeNum;
-    rValue.textContent = `New Grid Size: ${gridSize}`;
+const newButton = document.querySelector(".rangeButton");
+const resButton = document.querySelector(".resetButton");
+let rangeNum = 16;
+    rValue.textContent = `Grid Size: ${gridSize}x${gridSize}`;
 
     rSlider.addEventListener("input", (event) => {
      rangeNum = event.target.value;
-     rValue.textContent = `New Grid Size: ${rangeNum}x${rangeNum}`;
+     rValue.textContent = `Grid Size: ${rangeNum}x${rangeNum}`;
     });
 
-    rButton.addEventListener("click", () =>{
-        gridSize = rValue.textContent;
-        console.log(gridSize);
+    newButton.addEventListener("click", () =>{
+        gridSize = rangeNum;
+        newGrid(gridSize);
+    });
+
+    resButton.addEventListener("click", () =>{
+        rSlider.value = gridSize;
+        rValue.textContent = `Grid Size: ${gridSize}x${gridSize}`
+        resetGrid(gridSize);
     }) 
+}
+
+
+function newGrid(size){
+
+}
+
+function resetGrid(size){
+
 }
